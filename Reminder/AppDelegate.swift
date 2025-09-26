cea13fceaf7d589c6279f320ef9414efe2a24411
lfelipe.dev <129457138+LuizFelipeCouto@users.noreply.github.com>
2025-09-25 22:48:05 -0300

    feat(login): integrate Firebase Authentication and implement login flow
    - Add ViewModel for LoginBottomSheet
    - Create delegate for communication between ViewController and ViewModel
    - Implement authentication using Firebase Authentication (signIn)
    - Configure Firebase in the project via Podfile and GoogleService-Info.plist
    - Add debug prints for login success/error

--- Reminder/AppDelegate.swift
+++ Reminder/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // Executado na inicialização do app
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -11,5 +11,12 @@ import Firebase
 class LoginBottomSheetViewModel {
     func doAuth(usernameLogin: String, password: String) {
         print(usernameLogin)
+        Auth.auth().signIn(withEmail: usernameLogin, password: password) { [weak self] (authResult, error) in
+            if let error = error {
+                print("Autenticacao nao foi com sucesso \(error)")
+            } else {
+                print(authResult, "Sucesso na autenticacao")
+            }
+        }
     }
 }
